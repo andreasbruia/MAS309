3e59a450e76a7ac9f51efc288a93fee0b69314ac
andreasbruia <andreasbr@uia.no>
2025-02-06 09:35:03 +0100

    trykk og moment på winch

--- MATLAB - håndberegninger/Regning av bolter/Kraft_bolter.m
+++ MATLAB - håndberegninger/Regning av bolter/Kraft_bolter.m
@@ -39,17 +39,3 @@ Ffrik = F_i*my
 
 Sikkerhetsfaktor = Ffrik/Vbolt
 
-
-
-%% Kladd for trykk
-clc;
-m = 250; %kg
-g = 9.81; %m/s^2
-r = 0.1588/2; %m
-D = 180*10^(-6); %m^3
-
-delta_p = (m*g*r*2*pi)/D
-p_bar = delta_p * 10^(-5)
-
-trykk = 70*10^5;
-Masse = (D*trykk)/(2*pi*g*r)
